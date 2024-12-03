5796ec3ea7445ddb0a9e94b4cf08d669e2e0d8ce
Jamie Potepa <jamie.potepa@cognizant.com>
2024-12-03 16:03:52 +0000

    Building out the modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 // import { useState } from "react";
-import "./App.css";
+// import "./App.css";
+import { useState } from "react";
 import { RxCross1 } from "react-icons/rx";
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <div>
       <div className="header">
@@ -11,7 +14,10 @@ function App() {
             <h1>Simple Notes</h1>
           </div>
           <div className="header__actions">
-            <button className="header__button button">
+            <button
+              className="header__button button"
+              onClick={() => setShowModal(true)}
+            >
               Add note
               <RxCross1 className="header__button-icon" />
             </button>
@@ -43,6 +49,7 @@ function App() {
           <Note />
         </div>
       </div>
+      {showModal && <Modal onClose={() => setShowModal(false)} />}
     </div>
   );
 }
@@ -61,6 +68,57 @@ function Note() {
       <div className="note__actions">
         <button className="note__button note__button--edit">Edit</button>
         <button className="note__button note__button--delete">Delete</button>
+      </div>
+    </div>
+  );
+}
+
+type ModalProps = {
+  onClose: () => void;
+  // onSave: () => void;
+};
+
+function Modal({ onClose }: ModalProps) {
+  // // ! DO I NEED THIS OR CAN I JUST INLINE IT?
+  // const handleContentClick = (e: MouseEvent<HTMLDivElement>) => {
+  //   e.stopPropagation();
+  // };
+
+  const handleSave = () => {
+    console.log("saved");
+  };
+
+  return (
+    <div className="modal">
+      <div className="modal__overlay" onClick={onClose}></div>
+      <div className="modal__content">
+        <button className="modal__close" onClick={onClose}>
+          <RxCross1 />
+        </button>
+        <div className="modal__header">
+          <h2>Add a new note</h2>
+        </div>
+        <div className="modal__body">
+          <form>
+            <input
+              type="text"
+              placeholder="Title ..."
+              className="modal__input"
+            />
+            <textarea
+              placeholder="Note content ..."
+              className="modal__text-area"
+            ></textarea>
+          </form>
+        </div>
+        <div className="modal__footer">
+          {/* <button className="modal__button modal__button--cancel">
+            Cancel
+          </button> */}
+          <button className="modal__button" onClick={handleSave}>
+            Save
+          </button>
+        </div>
       </div>
     </div>
   );
