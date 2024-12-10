f878c126da4f57b89e087f29d7d95cf482e79db6
Jamie Potepa <jamie.potepa@cognizant.com>
2024-12-10 12:07:53 +0000

    removing original search text

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -26,34 +26,3 @@ const Search = (
 
 const SearchWithForwardRef = forwardRef<HTMLInputElement, SearchProps>(Search);
 export default SearchWithForwardRef;
-
-// Original code without forward ref
-// import { ChangeEvent } from "react";
-
-// import "./search.scss";
-
-// type SearchAndFilterProps = {
-//   handleSearchOnChange: (e: ChangeEvent<HTMLInputElement>) => void;
-// };
-
-// function SearchAndFilter({ handleSearchOnChange }: SearchAndFilterProps) {
-//   return (
-//     <div className="search">
-//       <input
-//         type="text"
-//         placeholder="Search notes..."
-//         className="search__input"
-//         onChange={handleSearchOnChange}
-//       />
-
-//       {/* <div className="search-and-filter__filter">
-//         <select>
-//           <option value="all">All</option>
-//           <option value="important">Important</option>
-//         </select>
-//       </div> */}
-//     </div>
-//   );
-// }
-
-// export default SearchAndFilter;
