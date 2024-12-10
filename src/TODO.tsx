4a7a54a9a23aebdb2daf3496276d7e56c9540cc7
Jamie Potepa <jamie.potepa@cognizant.com>
2024-12-10 11:42:41 +0000

    using separate API functions, moving TODO to it's own file. Updating modal

---
+++ src/TODO.tsx
@@ -0,0 +1,32 @@
+// TODO
+// Need error handling if a fetch fails. Some sort of message. !ok
+// Move fetches to hooks?
+// Add edit note functionality - If I do, make sure to change the submit button text to read update
+// update Readme to explain how to run the app and give some explanation of the code
+// Change search-and-filter naming to search if I don't implement the filter
+// Clean up debounce if component gets unmounted?
+//? Maybe the filter could switch between searching the title of content of each note
+// add type button to all button that are not submit
+// update modal to use children so I have a reusable modal component - I could use this for the delete note modal to say, are you sure you want to delete this note?
+
+// Russells suggestions
+// Potentially add a couple of unit tests to show I can
+// Separate network requests into their own file outside of the hooks
+// Discriminated unions for the fetch requests return types
+// Add notes section to own component and use early returns - if you do this, set up context for the notes
+// maybe add errors as a type to the fetch requests
+// Add con currently so one npm commmand starts frontend and backend
+
+// README
+// Explain how to run the app.
+// Explain 5000 error and what needs to be changed if that port is not available
+// Add animation to modal open and close
+// Change name of this component to be more generic AddNoteForm
+
+//? Check typescript for onClick events etc
+
+//* Separate components and styles
+//* Cancel button in the modal needs to clear the form and close the modal
+//* Take out case for search
+//* Add search functionality - use Debounce
+//* Put header section and filter into their own components?
